61e600d17622b44ee1958f55e8bf4efbeb44ad9b
castro3 <castroxoscar@gmail.com>
2020-09-27 03:34:56 -0600

    Added server and client code

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -1,2 +1,44 @@
 #Oscar Castro
 #!/usr/bin/env python3
+
+# Client program
+import socket, sys, re
+
+sys.path.append("../lib") # for params
+import params
+
+from framedSock import frameSend, framedReceive
+
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-p', '--proxy'), "proxy", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+paramMap = params.parseParams(switchesVarDefaults)
+server, proxy, usage = paramMap['server'], paramMap['proxy'],paramMap['usage']
+
+if usage:
+    params.usage()
+
+try:
+    serverHost, serverPort = re.split(':', server)
+    serverPort = int(serverPort)
+
+except:
+    print('ERROR: Can\'t parse server:port from %s' %server)
+    sys.exit(1)
+
+
+addrFamily = socket.AF_INET
+socktype = socket.SOCK_STREAM
+addrPort = (serverHost, serverPort)
+
+clientSocket = socket.socket(addrFamily, socktype)
+
+if clientSocket is None:
+    print('Could not open socket')
+    sys.exit(1)
+
+clientSocket.connect(addrPort)
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -1,2 +1,43 @@
 #Oscar Castro
 #!/usr/bin/env python3
+
+import socket, re, sys, os
+
+sys.path.append('../lib')
+
+import params
+
+sys.path.append('../framed-echo')
+
+from framedSock import framedReceive, framedSend
+
+
+
+
+
+switchesVarDefaults = (
+    (('-l', '--listentPort'), 'listenPort', 50001),
+    (('-p', '--proxy'), 'proxy', False),
+    (('-?', '--usage'), 'usage', False)
+    )
+
+
+paramMap = params.parseParams(switchesVarDefaults)
+listenPort, proxy, usage = paramMap['listenPort'], paramMap['proxy'], paramMap['usage']
+
+
+if usage:
+    params.usage()
+
+
+file = {}
+token = '0'
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+bindAddr = ("127.0.0.1", listenPort)
+serverSocket.bind(bindAddr)
+serverSocket.listen(1)
+print("listening on:", bindAddr)
+
+sock, addr = serverSocket.accept()
+
+
