d5bd29be8a05892798561da9c0bd0d226141e23a
castro3 <castroxoscar@gmail.com>
2020-09-28 03:13:16 -0600

    Able to send and recieve files but certain errors occur

--- file-transfer-lab/fileClient.py
+++ file-transfer-lab/fileClient.py
@@ -4,10 +4,11 @@
 # Client program
 import socket, sys, re
 
-sys.path.append("../lib") # for params
+sys.path.append('../lib') # for params
 import params
 
-from framedSock import frameSend, framedReceive
+sys.path.append('../framed-echo')
+from framedSock import framedSend, framedReceive
 
 
 switchesVarDefaults = (
@@ -42,3 +43,50 @@ if clientSocket is None:
     sys.exit(1)
 
 clientSocket.connect(addrPort)
+
+
+# loop through the server program
+while True:
+    try: # get a token to check if the connection worked
+        token = clientSocket.recv(1024).decode() if not proxy else framedSend(clientSocket, False)
+        print("Token: %s" %token)
+    except ConnectionResetError:
+        print("Lost connection to server...")
+        break
+
+    file = input('Enter name of file: ') # input from user
+    data = ''  # data that will be in the file provided
+    if file == 'exit':
+        clientSocket.send(file.encode()) if not proxy else frameSend(clientSocket,
+                                                                     file.encode(), False)
+        break
+
+    try: # make sure the file the user inputted exists
+        path = './' + file
+        fileInput = open(path, 'r')
+    except FileNotFoundError:
+        print("File %s does not exist" %file)
+        file = None
+        data = None
+
+    if data is not None: # add data
+        data = fileInput.readlines()
+        if fileInput:
+            for d in range(len(data)):
+                data[d] = token + ", " + file + ": " + data[d]
+
+            for info in data:
+                print("Sending: %s" %info, end='')
+                clientSocket.send(info.encode()) if not proxy else frameSend(clientSocket,
+                                                                             info.encode(), False)
+            fileInput.close() # finish with file
+            token = str(int(token) + 1)
+            clientSocket.send(token.encode()) if not proxy else framedSend(clientSocket,
+                                                                           token.encode(), False)
+
+    else:
+        print("File %s does not exist or has no data" %file)
+        clientSocket.send(token.encode()) if not proxy else framedSend(clientSocket,
+                                                                       token.encode(), False)
+            
+clientSocket.close()
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -12,9 +12,6 @@ sys.path.append('../framed-echo')
 from framedSock import framedReceive, framedSend
 
 
-
-
-
 switchesVarDefaults = (
     (('-l', '--listentPort'), 'listenPort', 50001),
     (('-p', '--proxy'), 'proxy', False),
@@ -30,14 +27,55 @@ if usage:
     params.usage()
 
 
-file = {}
+files = {}
+file = ''
 token = '0'
+fileData = ''
+
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-bindAddr = ("127.0.0.1", listenPort)
+bindAddr = ("", listenPort)
 serverSocket.bind(bindAddr)
 serverSocket.listen(1)
-print("listening on:", bindAddr)
+print("listening to client...")
 
 sock, addr = serverSocket.accept()
+sock.send(token.encode()) if not proxy else framedSend(sock, token.encode(), False)
+
+# loop through server receiving files
+while True:
+    data = sock.recv(1024).decode() if not proxy else framedSend(sock, False)
+    if not data:
+        if file not in files:
+            print("File not found")
+        break
+
+    if proxy:
+        data = data.decode()
+
+    if data == "exit":
+        break
+
+    infoData = re.split(":", data)
+    if len(infoData) == 1:
+        token = infoData[0]
+        files[file] = -1
+        file = None
+        fileData = None
+        sock.send(token.encode()) if not proxy else framedSend(sock, token.encode(), False)
+        print("Sending... Token: " + token)
+
+    else:
+        print(infoData)
+        token = infoData[0]
+        file = infoData[1]
+        fileData = infoData[2]
+
+    if file is None:
+        path = './' + file
+        file = open(path, 'a')
+
+        file.write(fileData)
+        file.close()
 
 
+sock.close()
